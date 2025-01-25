db7beb6373e276b87be2c27eac9df7d1be20424c
Esther Moon <113371188+rmoon45@users.noreply.github.com>
2025-01-24 23:50:17 -0500

    ok like timer frfr i think?

--- Assets/Scripts/SelectionScreenManager.cs
+++ Assets/Scripts/SelectionScreenManager.cs
@@ -7,26 +7,19 @@ public class SelectionScreenManager : MonoBehaviour
 
     [SerializeField] private GameObject investorScreen;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    [SerializeField] private GameObject timeManager;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     public void ActivateMakerScreen() {    
         makerScreen.SetActive(true);
+        timeManager.SetActive(true);
         gameObject.SetActive(false);
 
     }
 
     public void ActivateInvestorScreen() {
         investorScreen.SetActive(true);
+        timeManager.SetActive(true);
         gameObject.SetActive(false);
     }
 }
--- Assets/Scripts/TimeManager.cs
+++ Assets/Scripts/TimeManager.cs
@@ -2,11 +2,16 @@ using UnityEngine;
 using TMPro;
 using System;
 
-public class TimerCountdown : MonoBehaviour
+/// <summary>
+///  Handles the time countdown system bc we want only one of them yes yes
+/// </summary>
+public class TimeManager : MonoBehaviour
 {
+    [SerializeField] TextMeshProUGUI timerTextMaker;
 
-    [SerializeField] TextMeshProUGUI timerText;
+    [SerializeField] TextMeshProUGUI timerTextInvestor;
     [SerializeField] float timerLength;
+    
 
     public event EventHandler OnEndTime;
 
@@ -24,14 +29,15 @@ public class TimerCountdown : MonoBehaviour
         int minutes = Mathf.FloorToInt(timer / 60);
         int seconds = Mathf.FloorToInt(timer % 60);
 
-        timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+        timerTextMaker.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+        timerTextInvestor.text = string.Format("{0:00}:{1:00}", minutes, seconds);
 
         if (timer > 0) {
             timer -= Time.deltaTime;   
 
         } else if (timer < 0) {
             timer = timerLength;
-            // fire off event
+            // fire off event ONCE YAYYYY
             OnEndTime?.Invoke(this, EventArgs.Empty);
         }
 
