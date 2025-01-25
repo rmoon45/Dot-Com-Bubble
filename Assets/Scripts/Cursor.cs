4448ca2914404b9cbfa908acc0c37a731349e990
Alex Yang <alex@yang2k.com>
2025-01-25 12:22:36 -0500

    Power button

--- Assets/Scripts/Cursor.cs
+++ Assets/Scripts/Cursor.cs
@@ -1,6 +1,25 @@
+using System;
 using UnityEngine;
 
 public class Cursor : MonoBehaviour
 {
+    [SerializeField] Texture2D cursorMain;
+    [SerializeField] Texture2D cursorPoint;
+    [SerializeField] Texture2D cursorText;
     
+    
+    private void Awake()
+    {
+        ChangeCursorMain();
+    }
+
+    public void ChangeCursorMain()
+    {
+        UnityEngine.Cursor.SetCursor(cursorMain, Vector2.zero, CursorMode.Auto);
+    }
+    
+    public void ChangeCursorPoint()
+    {
+        UnityEngine.Cursor.SetCursor(cursorPoint, Vector2.one * 10, CursorMode.Auto);
+    }
 }
