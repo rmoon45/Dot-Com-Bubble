45930a865734de4dcc674b51b59d52a81f55d004
Alex Yang <alex@yang2k.com>
2025-01-25 17:09:16 -0500

    Stuff

--- Assets/Scripts/TextModuleInit.cs
+++ Assets/Scripts/TextModuleInit.cs
@@ -4,15 +4,15 @@ using UnityEngine;
 
 namespace DefaultNamespace
 {
-    public class TextModuleInit
+    public class TextModuleInit : MonoBehaviour
     {
-        private string[] texts =
+        [SerializeField] private string[] texts =
         {
             "Jan 1. 1998 - hello! :) my name is des, and this right here is my very own corner of the internet dedicated to the things that i love. i mostly use it as a digital journal, of both my personal life and interests."
             
         };
 
-        [SerializeField] private TextMeshPro t;
+        [SerializeField] private TextMeshProUGUI t;
         
         void OnEnable()
         {
