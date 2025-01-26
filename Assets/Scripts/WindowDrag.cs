b4abcdc7faf04e249fc46c48681e9d452508adf9
Alex Yang <alex@yang2k.com>
2025-01-26 02:50:21 -0500

    Window drag bounds

--- Assets/Scripts/WindowDrag.cs
+++ Assets/Scripts/WindowDrag.cs
@@ -8,9 +8,13 @@ public class WindowDrag : MonoBehaviour, IDragHandler
     
     private Canvas canvas;
 
+    private Vector2 canvasSize;
+
     private void Awake()
     {
         canvas = GetComponentInParent<Canvas>();
+
+        canvasSize = canvas.GetComponent<RectTransform>().sizeDelta;
     }
 
     void IDragHandler.OnDrag(PointerEventData eventData)
@@ -22,5 +26,17 @@ public class WindowDrag : MonoBehaviour, IDragHandler
         }
         
         rectTransform.anchoredPosition += eventData.delta / canvas.scaleFactor;
+
+        var pos = rectTransform.anchoredPosition;
+        Vector2 halfSize = rectTransform.sizeDelta / 2;
+        float left = pos.x - halfSize.x;
+        float right = pos.x + halfSize.x;
+        float top = pos.y + halfSize.y;
+        float bottom = pos.y + halfSize.y;
+        
+        if (left < 0) rectTransform.anchoredPosition = new Vector2(halfSize.x, pos.y);
+        // print(right);
+        //if (right > 0) rectTransform.anchoredPosition = new Vector2(canvas.pixelRect.width / 2 - halfSize.x, pos.y);
+        rectTransform.anchoredPosition = new Vector2(canvasSize.x / 2 - halfSize.x, pos.y);
     }
 }
