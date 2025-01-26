1a2f0ad73e12be5a223b9a99e82666c626dde318
Esther Moon <113371188+rmoon45@users.noreply.github.com>
2025-01-26 02:42:26 -0500

    NewsManager and NewsStory functionality implemented. There is 0 consideration for offsetting the actual stories. lol

---
+++ Assets/Scripts/NewsManager.cs
@@ -0,0 +1,43 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewsManager : MonoBehaviour
+{
+    [SerializeField] GameObject newsStory;
+
+    [SerializeField] GameObject contentObject;
+
+    // create a list of gameobjects
+    List<NewsStory> newsStoryInstantiatedList;
+
+    void Start() {
+        newsStoryInstantiatedList = new List<NewsStory>();
+
+        SetNews(new string[] {"cries", "I'M GOING TO LIVE MYSELF"});
+    }
+
+    /// <summary>
+    /// Should take in array of strings and create the news system
+    /// </summary>
+    /// <param name="newsLines"></param>
+    public void SetNews(string[] newsLines) {
+        // clear out the prior newsStory stuff
+        foreach (NewsStory ns in newsStoryInstantiatedList) {
+            Destroy(ns);
+        }
+        newsStoryInstantiatedList.Clear();
+
+
+        // loop thru the array
+        foreach (string newL in newsLines) {
+            // should add a child under the Content parent
+            GameObject instantiatedNewsStory = Instantiate(newsStory, contentObject.transform);
+
+            // this should add it to a dynamic list of NewsStory (hopefully)
+            newsStoryInstantiatedList.Add(instantiatedNewsStory.GetComponent<NewsStory>());
+
+            // call the function to set the newsStoryText yay
+            instantiatedNewsStory.GetComponent<NewsStory>().SetNewsStoryText(newL);
+        }
+    }
+}
